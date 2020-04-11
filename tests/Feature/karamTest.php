11c45000509db53d8e90bfdffd2b97abdf443c46
karam mustafa <karam2mustafa@gmail.com>
2020-04-11 03:18:46 +0300

    update

---
+++ tests/Feature/karamTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace KaramTools\Press\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class karamTest extends TestCase
+{
+
+    
+    public function test_my_test_work()
+    {
+        $this->assertTrue(true);
+    }
+}
